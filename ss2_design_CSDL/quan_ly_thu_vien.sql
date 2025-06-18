321bdf972c71c1c8d3e587009154c2ead1d9e865
unknown <le17112401@gmail.com>
2025-06-18 16:12:35 +0700

    fixed ss2

--- ss2_design_CSDL/quan_ly_thu_vien.sql
+++ ss2_design_CSDL/quan_ly_thu_vien.sql
@@ -3,33 +3,33 @@ use quan_ly_thu_vien;
 
 create table students(
 id_student int  auto_increment primary key,
-name_student varchar(100),
-birthday varchar(100),
-class_name varchar(50)
+name_student varchar(100) not null,
+birthday varchar(15) not null,
+class_name varchar(50) not null
 );
-create table category(
+create table categories(
 id_category int  auto_increment primary key,
-name_category varchar(100)
+name_category varchar(100) not null
 );
 create table authors(
-id_authors int auto_increment primary key,
-name_authors varchar(100)
+id_author int auto_increment primary key,
+name_author varchar(100) not null
 );
 create table books(
 id_book int  auto_increment primary key,
-title varchar(100),
-page_size int,
-id_category int,
-id_authors int ,
+title varchar(100) not null,
+page_size int not null,
+id_category int ,
+id_author int ,
 foreign key (id_category) references category(id_category),
-foreign key (id_authors) references authors(id_authors)
+foreign key (id_author) references authors(id_author)
 );
 create table borrows(
 id_borow int auto_increment primary key,
-id_student int,
-id_book int,
-borrow_date varchar(50),
-return_date varchar(50),
+id_student int ,
+id_book int ,
+borrow_date varchar(50) not null,
+return_date varchar(50) not null,
 foreign key (id_student) references students(id_student),
 foreign key (id_book) references books(id_book)
 );
