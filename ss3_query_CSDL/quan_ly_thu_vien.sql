0a4c2faa448036b8c4b5fa7d030965852bdaa5ec
unknown <le17112401@gmail.com>
2025-06-19 11:51:33 +0700

    fixed

--- ss3_query_CSDL/quan_ly_thu_vien.sql
+++ ss3_query_CSDL/quan_ly_thu_vien.sql
@@ -40,13 +40,16 @@ select* from students;
 select* from books;
 select* from borrows;
 
+-- + Lấy ra toàn bộ sách có trong thư viện, cùng với tên thể loại và tác giả					
 select b.title, a.name_authors,c.name_category  from books b join authors a on b.id_authors=a.id_authors
 join category c on b.id_category=c.id_category;
 
+-- + Lấy ra danh sách các học viên đã từng mượn sách và sắp xếp danh sách theo theo tên từ a->z						
 select distinct s.id_student, s.name_student
 from students s join borrows b on s.id_student=b.id_student
 order by s.name_student;
 
+-- + Lấy ra  2 quyển sách được mượn nhiều nhất				
 select count(bor.id_book) as so_lan_muon, b.title
  from borrows bor join books b on bor.id_book=b.id_book
  group by bor.id_book
